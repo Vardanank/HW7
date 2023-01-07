169346c4a25a7550c138b7bc847ebbcdf70263d7
Vardanank <vardan.nahapetyan.8998@gmail.com>
2023-01-07 12:48:19 +0300

    commit test1 end

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
@@ -5,7 +7,18 @@ public class Main {
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
 
+        boolean havePoint = false;
+
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("введите количество Жабьих глаз");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.println("введите количество Слез вурдалака");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+        System.out.println("введите количество Костей ворона");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.println("введите количество Пельмень");
+        dumplingsCount = new Scanner(System.in).nextInt();
+
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -14,5 +27,27 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+
+        if (toadEyesCount >= 3 && ravenBonesCount >= 1) {
+            System.out.println("вы можете сделать Эликсир зоркости");
+            havePoint = true;
+        }
+        if (ravenBonesCount >= 2 && dumplingsCount >= 4) {
+            System.out.println("вы можете сделать Эликсир стойкости");
+            havePoint = true;
+        }
+        if (ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2) {
+            System.out.println("вы можете сделать Эликсир скрытности");
+            havePoint = true;
+        }
+        if (ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 4 && ravenBonesCount >= 4) {
+            System.out.println("вы можете сделать Запретный Эликсир");
+            havePoint = true;
+        }
+
+        if (!havePoint){
+            System.out.println("вы не можете сделать Эликсир");
+        }
+
     }
 }
--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+//        for (int i = 2; i <= 100; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println("уиолен айтишник с id " + i);
+//            }
+//        }
+
+        int i = 2;
+        while (i  <= 100) {
+            if (i % 2 == 0) {
+                System.out.println("уиолен айтишник с id " + i);
+            }
+            i++;
+        }
     }
 }
